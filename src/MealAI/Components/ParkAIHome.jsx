64cf1d0031b64c50825cec89b1720c6c3b579db4
Michael Geraghty <tup13771@temple.edu>
2024-12-05 13:46:35 -0500

    imported usecallback

--- src/MealAI/Components/ParkAIHome.jsx
+++ src/MealAI/Components/ParkAIHome.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import '../../Style/parkAI.css';
 import { fetchChatGPTResponse } from '../Functionality/FetchChatGPT';
 import defaultImage from '../Assets/default_img.jpg';
